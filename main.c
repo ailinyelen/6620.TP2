5b4fa8007a38aed4d168a814dc09984c6c10570a
ailinyelen <33434576+ailinyelen@users.noreply.github.com>
2021-11-30 23:59:19 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define BYTE_MEMORIA 1024
+#include <string.h>
+#include <math.h>
 
-
-/*
-	Instalador Word to Telax
-	
-	https://www.grindeq.com/index.php?p=download&lang=en
-	
-	
-	
-	https://www.youtube.com/watch?v=nMjpFHBg_1Q
-	https://getintopc.com/softwares/utilities/grindeq-math-utilities-2020-free-download/
-*/
-// PAGINA SALVADORA,
-// https://products.aspose.app/pdf/es/conversion/docx-to-tex
+#define ERROR -1
+#define BYTE_MEMORIA 65536
+#define BITS_DIR_MEM 16
 
 
 char * ByteMemoria;
@@ -111,7 +102,7 @@ void end()
 
 /*
 La funcion find set(unsigned int address) debe devolver el conjunto
-de cache al que mapea la direccion address.
+de cache al que mapea la direccion address.
 */
 unsigned int find_offset(unsigned int address)
 {
@@ -126,7 +117,7 @@ unsigned int find_offset(unsigned int address)
 
 /*
 La funcion find set(unsigned int address) debe devolver el conjunto
-de cache al que mapea la direccion address.
+de cache al que mapea la direccion address.
 */
 unsigned int find_set(unsigned int address)
 {
@@ -140,7 +131,7 @@ unsigned int find_set(unsigned int address)
 
 /*
 La funcion find set(unsigned int address) debe devolver el conjunto
-de cache al que mapea la direccion address.
+de cache al que mapea la direccion address.
 */
 unsigned int find_tag(unsigned int address)
 {	
@@ -151,7 +142,7 @@ unsigned int find_tag(unsigned int address)
 
 /*
 La funcion find block(unsigned int address) debe devolver el bloque
-de memoria correspondiente a la direccion address.
+de memoria correspondiente a la direccion address.
 */
 
 unsigned int find_block (unsigned int address)
@@ -366,7 +357,7 @@ char read_byte(unsigned int address, unsigned char *hit)
 
 /*
 La funcion write byte(unsigned int address, char value) debe
-escribir el valor value en la posicion correcta del bloque que corresponde
+escribir el valor value en la posicion correcta del bloque que corresponde
 a address, si esta en el cache, y escribir 1 en *hit si es un hit
 y 0 si es un miss.
 */
@@ -389,12 +380,12 @@ void write_byte(unsigned int address, char value, unsigned char *hit)
 		read_block(block);
 		
 		// MARCA SALIDA
-		hit[0] = 1;
+		hit[0] = 0;
 	}
 	else
 	{
 		// MARCA SALIDA
-		hit[0] = 0;
+		hit[0] = 1;
 	}
 
 	memoria.Bloque[set].vias[encontradoenvia].is_dirty = 1;
@@ -404,9 +395,9 @@ void write_byte(unsigned int address, char value, unsigned char *hit)
 
 /*
 La funcion get miss rate() debe devolver el porcentaje de misses
-desde que se inicializo el cache.
-read byte() y write byte() solo deben interactuar con la memoria
-a traves de las otras primitivas.
+desde que se inicializo el cache.
+read byte() y write byte() solo deben interactuar con la memoria
+a traves de las otras primitivas.
 */
 char get_miss_rate()
 {
@@ -445,63 +436,132 @@ void mostrarcache()
 
 
 
-int main(int argc, char *argv[]) 
-{
+int main(int argc, char *argv[]) {
 
-	char miss = 0;
-	memoria.bitsoffset = 4;
-	memoria.capaciadbloques = 16;
-	memoria.bitsindex = 4;
-	memoria.cantidadbloques = 16;
-	memoria.cantidadvias = 2;
-	memoria.bitstag = 24;
+	char* output_file_name = NULL;
+    int ways = -1, block_size = -1, cache_size = -1;
+
+    for (int i = 1; i < argc-2; i++) {
+
+		if (strcmp(argv[i],"-h") == 0 || strcmp(argv[i] , "--help") == 0) {
+			fprintf (stdout, "Usage: \n tp2 -h \n tp2 -V \n tp2 options archivo \nOptions: \n -h, --help  ~Imprime ayuda.\n");
+			fprintf (stdout, " -V, --version ~Version del programa.\n -o, --output ~Archivo de salida.\n");
+			fprintf (stdout, " -w, --ways ~Cantidad de vias.\n -c, --cachesize ~Cantidad de kilobytes del cache.");
+			fprintf (stdout, "\n -b, --blocksize ~Cantidad de bytes del bloque.\nExamples:\n tp2 -w 4 -cs 8 -bs 16 prueba1.mem\n");
+			return 0;
+		}
+		else if (strcmp(argv[i] , "-V") == 0 || strcmp(argv[i] , "--version") == 0) {
+			fprintf (stdout, "Version 1.0\n");
+			return 0;
+		}
+		else if (strcmp(argv[i] , "-o") == 0 || strcmp(argv[i] , "--output") == 0) {
+			i++;
+			output_file_name = argv[i];
+		}
+		else if (strcmp(argv[i] , "-w") == 0 || strcmp(argv[i] , "--ways") == 0) {
+			i++;
+			ways = atoi(argv[i]);
+		}
+		else if (strcmp(argv[i] , "-cs") == 0 || strcmp(argv[i] , "--cachesize") == 0) {
+			i++;
+			int argument = atoi(argv[i]);
+			if (argument <= 0) {
+				fprintf (stderr, "ERROR: bad argument input, cache size can not be equal or less than 0\n");
+				return ERROR;
+			}
+			int bits = 10 + (log(argument)/log(2));
+			cache_size = pow(2, bits);
+		}
+		else if (strcmp(argv[i] , "-bs") == 0 || strcmp(argv[i] , "--blocksize") == 0) {
+			i++;
+			block_size = atoi(argv[i]);
+		}
+		else {
+			fprintf (stdout, "Use -h or --help if needed.\n");
+			return ERROR;
+		}
+	}
+
+    if (ways <= 0 || cache_size <= 0 || block_size <= 0 ) {
+        fprintf (stderr, "ERROR: bad arguments input\n");
+        return ERROR;
+    }
+
+	memoria.capaciadbloques = block_size;
+	memoria.cantidadvias = ways;
+    int conjuntos = (cache_size / block_size) / ways;
+	memoria.cantidadbloques = conjuntos;
+	int bits_offset = log((double)block_size)/log((double)2);
+	int bits_index = log((double)conjuntos)/log((double)2);
+	memoria.bitsoffset = bits_offset; 
+	memoria.bitsindex = bits_index;
+    int bits_tag = BITS_DIR_MEM - bits_index - bits_offset;
+	memoria.bitstag = bits_tag;
 	
-	
+	//PRINTS PARA CHEQUEO - ELIMINAR
+    fprintf (stdout, "cache de %d Bytes\n", cache_size);
+    fprintf (stdout, "#conjuntos %d de %d Bytes cada bloque\n", conjuntos, block_size);
+    fprintf (stdout, "#vias %d\n ", ways);
+    fprintf (stdout, "%d bits de TAG, %d bits de INDEX, %d bits de OFFSET\n", bits_tag, bits_index, bits_offset);
+    
 	ByteMemoria = malloc(sizeof(char) * BYTE_MEMORIA);
 
-	init();
-	
-	write_byte(256, 1, &miss);
-	//read_byte(256, &miss);
-	read_byte(512, &miss);
-	read_byte(1024, &miss);
-	
-	mostrarcache();
-	
-	
-	/*
-	mostrarcache();
-	read_byte(0, &miss);
-	
-	read_byte(256, &miss);
-	mostrarcache();
-	
-	read_byte(512, &miss);
-	mostrarcache();
-	read_byte(768, &miss);
-	mostrarcache();
-	
-	read_byte(1024, &miss);
-	mostrarcache();
-	*/
-	/*
+    char* input_file_name = argv[argc-1]; 
+    FILE* input_file = fopen(input_file_name, "r");
+    if (!input_file) {
+        fprintf(stderr, "Error while opening input file %s\n", input_file_name);
+        return 1;
+    }
 
-	UN MISS Y UN HIT
-	---------------
-	
-	read_byte(0, &miss);
-	read_byte(1, &miss);
-	end();
-	*/
-	
+	if (output_file_name) {
+		FILE* output_file = fopen(output_file_name, "w");
+		if (!output_file) {
+			fprintf(stderr, "Error while opening output file %s\n", output_file_name);
+			return 1;
+		}
+		stdout = output_file;
+		stderr = output_file;
+	}
+
+    char x[6];
+	int memory_pos;
+	unsigned char hit = 0;
+
+    while (fscanf(input_file, "%5s", x)!= EOF) {
+        if (strcmp(x, "init") == 0) 
+		init();
+		else if (strcmp(x, "R") == 0) {
+			fscanf(input_file, "%d", &memory_pos);
+			//PRINTS PARA CHEQUEO - ELIMINAR
+			fprintf(stdout, "R %d\n", memory_pos);
+			char data = read_byte(memory_pos, &hit);
+			fprintf (stdout, "data: %d", data);
+			if (hit == 1) fprintf(stdout, " -HIT\n");
+			else fprintf (stdout, " -MISS\n");
+		}
+		else if (strcmp(x, "W") == 0) {
+			
+			fscanf(input_file, "%7s", x);
+			char* number = strtok(x, ",");
+			memory_pos = atoi(number);
+			
+			int value;
+			fscanf(input_file, "%d", &value);
+			//PRINTS PARA CHEQUEO - ELIMINAR
+			fprintf(stdout, "W %d %d\n", memory_pos, value);
+			
+			write_byte(memory_pos, value, &hit);
+			if (hit == 1) fprintf(stdout, "HIT\n");
+			else fprintf (stdout, "MISS\n");
+		}
+		else if (strcmp(x, "MR") == 0) {			
+			char mr = get_miss_rate();
+			fprintf(stdout, "MISS RATE: %d\n", mr);
+		}
+    }
+
+    fclose(input_file);
 	free(ByteMemoria);
-	/*
-	*/
-	
-	
-
-
 
 	return 0;
 }
-
